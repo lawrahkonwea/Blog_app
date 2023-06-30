4bd06aa7206befe56ce6a0f41706923341182e98
lawrahkonwea <lawrahcollections@gmail.com>
2023-06-30 04:51:27 +0100

    Add validation, method and relationship in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
+
+  after_save :update_posts_count
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def update_posts_count
+    author.increment!(:post_counter)
+  end
+
+  def recent_five_comments
+    comments.last(5)
+  end
 end
